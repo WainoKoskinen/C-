1df082cf34fa60178f54e5fa48051bbea748bf8d
e2402230 <e2402230@edu.vamk.fi>
2026-02-04 17:20:11 +0200

    tunti 4 valmis

--- tunti4.cpp
+++ tunti4.cpp
@@ -1,23 +1,35 @@
-#include <iostream>
-#include <vector>
+//
+
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
-int main(){
-srand(time(0));
-int luku = rand() % 100 + 1;
-int summa = 0;
-double keskiarvo;
+int main()
+{
 
-for(int i = 0; i < 20; i++){
-    luku = rand() % 100 + 1;
-    summa += luku;
-    keskiarvo = summa / 20.0;
-    std::cout << i << "\n";
-} 
+    srand(time(0));
+    int luku = rand() % 100 + 1;
+    int summa = 0;
+    double keskiarvo;
+    std::cout << "moi\n";
+    std::cout << luku << "\n";
 
+    for (int i = 0; i < luku; i++)
+    {
+        summa += i;
+    }
+
+    keskiarvo = static_cast<double>(summa) / luku;
+
+    if (luku % 2 == 0)
+    {
+        std::cout << luku << "luku on parillinen\n";
+    }
+    else
+    {
+        std::cout << luku << "luku on pariton\n";
+    }
 
     std::cout << summa << "\n";
     std::cout << keskiarvo << "\n";
-
 }
